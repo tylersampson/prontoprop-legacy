e37d708545e9db9a08b4792120234754159e7484
Tyler Sampson <tyler_sampson@tresblue.com>
2016-02-21 20:17:20 +0200

    Deployment

---
+++ config/initializers/bower_rails.rb
@@ -0,0 +1,6 @@
+BowerRails.configure do |bower_rails|
+
+  # Passes the -F option to rake bower:install or rake bower:install:deployment. Defaults to false.
+  bower_rails.force_install = true
+
+end
