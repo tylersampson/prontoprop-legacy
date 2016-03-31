196494d2dc8f1618e29776a7da4be48b81d9057e
Tyler Sampson <tyler_sampson@tresblue.com>
2016-03-31 23:42:18 +0000

    Fix host for production.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server '41.185.30.167', user: 'deploy', roles: %w{app web}
+server '188.166.171.65', user: 'deploy', roles: %w{app web}
 
 
 
