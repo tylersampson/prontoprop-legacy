b32e29142dcf33f0b78ec60bcb7720381c5e8944
Tyler Sampson <tyler_sampson@tresblue.com>
2016-04-01 01:00:13 +0000

    Remove sendgrid

--- config/initializers/send_grid.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USER'],
-  :password => ENV['SENDGRID_PASS'],
-  :domain => 'prontoprop.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
