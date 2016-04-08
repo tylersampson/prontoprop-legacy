6f2594bd38b525d1bdf4b46fb0a975b08e8b59ff
Tyler Sampson <tyler_sampson@tresblue.com>
2016-04-08 13:25:43 +0000

    Force rollbar key

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -2,7 +2,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
+  config.access_token = '91c72bcda9404ebabc80b6b6d848d7a6'
 
   # Here we'll disable in 'test':
   if Rails.env.test?
